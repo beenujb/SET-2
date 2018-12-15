1454c13d10455e1725d721e571925c8ac49f0833
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-15 12:44:48 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
 num = 7
-factorial = 1
+fact= 1
 if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
+   print("Sorry, fact does not exist for negative numbers")
 elif num == 0:
-   print("The factorial of 0 is 1")
+   print("The fact of 0 is 1")
 else:
    for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+       fact = fact*i
+   print("The factof",num,"is",fact)
